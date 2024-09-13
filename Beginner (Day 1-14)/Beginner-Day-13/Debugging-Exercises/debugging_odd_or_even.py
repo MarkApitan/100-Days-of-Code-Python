9760b4f26ca29bce67fbc7dedc91f55bd9dd3f22
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-09-13 14:01:49 +0800

    Completed 2 debugging exercises

---
+++ Beginner (Day 1-14)/Beginner-Day-13/Debugging-Exercises/debugging_odd_or_even.py
@@ -0,0 +1,14 @@
+# Debugging Odd or Even
+# Modify the code to fix the program. Fix the code so that it works and passes the tests when you submit.
+
+# def odd_or_even(number):
+#   if number % 2 = 0:
+#       return "This is an even number."
+#   else:
+#       return "This is an odd number."
+
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."

74a91c0f66f24918de18d9e35f9fc1dd7783ddc6
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-09-17 23:55:06 +0800

    Finished day 16 main project but will continue tomorrow to add designs

--- Intermediate (Day 15-57)/Intermediate-Day-16/Main-Project/main.py
+++ Intermediate (Day 15-57)/Intermediate-Day-16/Main-Project/main.py
@@ -1,3 +1,22 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+menu = Menu()
+coffee_maker = CoffeeMaker()
+
+is_on = True
+while is_on:
+    order = input("What would you like? (espresso/latte/capuccino): ").lower()
+
+    if order == 'off':
+        print("Shutting Down!")
+        is_on = False
+    elif order == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)

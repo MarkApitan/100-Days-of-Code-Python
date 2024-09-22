54a11687851c93d4fa3a372330baa08a9e0e167e
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-09-22 22:41:14 +0800

    finished turtle challenge 4

--- Intermediate (Day 15-57)/Intermediate-Day-18/Exercises/turtle-challenge-four-random-walk.py
+++ Intermediate (Day 15-57)/Intermediate-Day-18/Exercises/turtle-challenge-four-random-walk.py
@@ -1,16 +1,19 @@
-"""Turtle Challenge 3: Drawing Different Shapes"""
+"""Turtle Challenge 4: Random Walk"""
 from turtle import Turtle, Screen
 import random
 timmy = Turtle()
 colors = ["cornflower blue", "gold", "red", "green", "violet", "indigo", "dark red", "medium spring green"]
+directions = [0, 90, 180, 270]
+# Change the thickness
+timmy.pensize(10)
+# Change the speed
+timmy.speed("fast")
 
-for i in range (3, 11):
+for i in range(100):
     color = random.choice(colors)
     timmy.color(f"{color}")
-    angle = 360/i
-    for num in range(i):
-        timmy.forward(100)
-        timmy.right(angle) 
-
+    timmy.forward(30)
+    timmy.setheading(random.choice(directions))
+    
 my_screen = Screen()
 my_screen.exitonclick()

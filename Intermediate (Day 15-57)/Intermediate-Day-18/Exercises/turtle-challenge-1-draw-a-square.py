c63a1b258284283231ce6469fa83dbf3991c1201
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-09-22 20:07:02 +0800

    finished turtle challenge 1

---
+++ Intermediate (Day 15-57)/Intermediate-Day-18/Exercises/turtle-challenge-1-draw-a-square.py
@@ -0,0 +1,15 @@
+"""Turtle Challenge 1: Draw a Square"""""
+
+from turtle import Turtle, Screen
+timmy = Turtle()
+timmy.shape("triangle")
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+
+my_screen = Screen()
+my_screen.exitonclick()

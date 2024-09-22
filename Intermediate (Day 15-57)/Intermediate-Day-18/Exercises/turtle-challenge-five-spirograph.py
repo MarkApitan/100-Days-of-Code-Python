95c06fa034a1c63647e5b70b567795ff94ede523
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-09-23 01:03:25 +0800

    fix

--- Intermediate (Day 15-57)/Intermediate-Day-18/Exercises/turtle-challenge-five-spirograph.py
+++ Intermediate (Day 15-57)/Intermediate-Day-18/Exercises/turtle-challenge-five-spirograph.py
@@ -1,6 +1,8 @@
 """Turtle Challenge 5: Draw a Spirograph"""
 from turtle import Turtle, Screen, colormode
 import random
+timmy = Turtle()
+colormode(255)
 
 def color_generator():
     r = random.randint(0, 255)
@@ -10,16 +12,14 @@ def color_generator():
     return random_color
 
 def draw_spinograph(size_of_gap):
-    timmy = Turtle()
-    colormode(255)
+
     timmy.speed("fastest")
     for i in range (int(360/ size_of_gap)):
         timmy.color(color_generator())
         timmy.circle(100)
         current_heading = timmy.heading()
         timmy.setheading(current_heading+size_of_gap)
-    my_screen = Screen()
-    my_screen.exitonclick()
 
-size_of_gap = int(input("Enter the size of gap that you want: "))
-draw_spinograph(size_of_gap)
+draw_spinograph(10)
+my_screen = Screen()
+my_screen.exitonclick()

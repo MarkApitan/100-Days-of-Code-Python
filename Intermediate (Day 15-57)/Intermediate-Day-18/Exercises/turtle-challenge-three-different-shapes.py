3e3d8d0aca24b6fceb7986369f28a369c40e755f
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-09-22 23:03:47 +0800

    added color generator

--- Intermediate (Day 15-57)/Intermediate-Day-18/Exercises/turtle-challenge-three-different-shapes.py
+++ Intermediate (Day 15-57)/Intermediate-Day-18/Exercises/turtle-challenge-three-different-shapes.py
@@ -1,12 +1,17 @@
 """Turtle Challenge 3: Drawing Different Shapes"""
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
 import random
 timmy = Turtle()
-colors = ["cornflower blue", "gold", "red", "green", "violet", "indigo", "dark red", "medium spring green"]
+colormode(255)
+def color_generator():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
 
 for i in range (3, 11):
-    color = random.choice(colors)
-    timmy.color(f"{color}")
+    timmy.color(color_generator())
     angle = 360/i
     for num in range(i):
         timmy.forward(100)

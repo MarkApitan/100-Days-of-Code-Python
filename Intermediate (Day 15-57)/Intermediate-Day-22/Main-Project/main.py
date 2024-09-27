d2cd93598d346232ed4dd6fe03c7f3e8f9df2a07
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-09-27 23:34:58 +0800

    create a paddle and move up and down

--- Intermediate (Day 15-57)/Intermediate-Day-22/Main-Project/main.py
+++ Intermediate (Day 15-57)/Intermediate-Day-22/Main-Project/main.py
@@ -1,10 +1,35 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
+STARTING_POSITION = (350, 0)
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
-screen.title("Arcade Game")
+screen.title("Pong Game")
+screen.tracer(0)
+
+paddle = Turtle("square")
+paddle.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle.penup()
+paddle.color("white")
+paddle.goto(STARTING_POSITION)
+paddle.showturtle()
+
+def up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
 
 
+screen.listen()
+screen.onkey(up, "Up")
+screen.onkey(down, "Down")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()

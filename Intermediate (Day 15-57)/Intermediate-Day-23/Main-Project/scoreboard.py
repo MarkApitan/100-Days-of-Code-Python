adba297d902515e346a838ef7c76fbf01ff4d5c6
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-10-01 22:40:52 +0800

    update level

--- Intermediate (Day 15-57)/Intermediate-Day-23/Main-Project/scoreboard.py
+++ Intermediate (Day 15-57)/Intermediate-Day-23/Main-Project/scoreboard.py
@@ -6,7 +6,7 @@ FONT = ("Courier", 24, "normal", 'bold')
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
+        self.score = 1
         self.color("black")
         self.hideturtle()
         self.penup()

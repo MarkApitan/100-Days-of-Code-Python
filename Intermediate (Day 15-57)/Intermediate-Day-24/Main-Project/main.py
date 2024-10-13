462cbbff5aabf777a3bc6d77224d7d2a8f8ef7e7
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-10-14 00:41:08 +0800

    Finished day 24 main project

--- Intermediate (Day 15-57)/Intermediate-Day-24/Main-Project/main.py
+++ Intermediate (Day 15-57)/Intermediate-Day-24/Main-Project/main.py
@@ -6,3 +6,13 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open('Input/Names/invited_names.txt', 'r') as names:
+    names_list = names.readlines()
+with open("Input/Letters/starting_letter.txt", 'r') as e:
+    contents = e.read()
+    for name in names_list:
+        name = name.strip("\n")
+        new_letter = contents.replace('[name]', f'{name}')
+        with open(f'Output/ReadyToSend/{name}.txt', 'w') as output:
+            output.write(new_letter)

c20a7090788a567b2b233ce96b130f1de378233f
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-10-13 11:54:43 +0800

    Finished updated snake game

--- Intermediate (Day 15-57)/Intermediate-Day-24/Updated Snake Game/scoreboard.py
+++ Intermediate (Day 15-57)/Intermediate-Day-24/Updated Snake Game/scoreboard.py
@@ -6,13 +6,14 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", "r") as self.f:
+            self.high_score = int(self.f.read())
         self.color("white")
         self.hideturtle()
         self.penup()
         self.goto(0, 275)
         self.show_score()
-    
+
     def show_score(self):
         self.clear()
         self.write(f"Score = {self.score}       Highscore = {self.high_score}", False, align = ALIGNMENT, font = FONT)
@@ -20,9 +21,15 @@ class Scoreboard(Turtle):
     def reset (self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", "w") as self.f:
+                self.f.write(f"{self.score}")
         self.score = 0
         self.show_score()
-        
+
+    def get_high_score(self):
+        with open("data.txt", "r") as self.f:
+            self.high_score = int(content)
+
     def add_score(self):
         self.score += 1
         self.show_score()

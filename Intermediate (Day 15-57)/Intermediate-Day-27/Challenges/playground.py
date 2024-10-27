e96438f329986558d6cc1da8e2ea02330238c4b6
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-10-27 17:07:43 +0800

    Finished day 27 second challenge

--- Intermediate (Day 15-57)/Intermediate-Day-27/Challenges/playground.py
+++ Intermediate (Day 15-57)/Intermediate-Day-27/Challenges/playground.py
@@ -1,10 +1,24 @@
 # Create a function that take an unlimited number of arguments
 # Use a loop to sum all the arguments inside the function
-def add (*args):
+# def add (*args):
     # return sum(args) (easy way)
-    sum = 0
-    for arg in args:
-        sum += arg
-    return sum
+    # sum = 0
+    # for arg in args:
+    #     sum += arg
+    # return sum
 
-print(add(1,2,3,4))
+# print(add(1,2,3,4))
+
+from tkinter import *
+root = Tk()
+root.geometry('500x500')
+my_label = Label(text = "I am a Label", font = ("Arial", 24, "bold"))
+my_label.pack()
+
+def button_clicked():
+    my_label.config(text = "I got clicked")
+
+button = Button(text = "Click Me", command = button_clicked)
+button.pack()
+
+root.mainloop()

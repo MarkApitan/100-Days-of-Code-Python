c628c20576df5a0d58a663f8193baf096f3175d0
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-10-27 22:55:19 +0800

    Finished day 27 fourth challenge

--- Intermediate (Day 15-57)/Intermediate-Day-27/Challenges/playground.py
+++ Intermediate (Day 15-57)/Intermediate-Day-27/Challenges/playground.py
@@ -13,15 +13,17 @@ from tkinter import *
 root = Tk()
 root.geometry('500x500')
 my_label = Label(text = "I am a Label", font = ("Arial", 24, "bold"))
-my_label.pack()
+my_label.grid(column = 0,row =0)
 
 def button_clicked():
     my_label.config(text = input.get())
 
 button = Button(text = "Click Me", command = button_clicked)
-button.pack()
+button.grid(column = 1,row =1)
 
+button_two = Button(text = "Click Me", command = button_clicked)
+button_two.grid(column =2,row =0)
 input = Entry()
-input.pack()
+input.grid(column =3,row =2)
 
 root.mainloop()

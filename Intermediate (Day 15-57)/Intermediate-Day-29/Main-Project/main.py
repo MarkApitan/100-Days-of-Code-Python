941a0b37c3f4a6072de688cfa23692da25896f6e
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-10-31 00:49:57 +0800

    added generate password functionality

--- Intermediate (Day 15-57)/Intermediate-Day-29/Main-Project/main.py
+++ Intermediate (Day 15-57)/Intermediate-Day-29/Main-Project/main.py
@@ -1,6 +1,27 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    password_entry.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    letters_list = [random.choice(letters) for item in range(random.randint(8, 10))]
+    symbols_list = [random.choice(symbols) for item in range(random.randint(2, 4))]
+    numbers_list = [random.choice(numbers) for item in range(random.randint(2, 4))]
+
+    password_list = letters_list + symbols_list + numbers_list
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
@@ -52,7 +73,7 @@ password_label.grid(column=0, row=3)
 password_entry = Entry(width=32)
 password_entry.grid(column=1, row=3)
 
-generate_password_button = Button(text='Generate Password')
+generate_password_button = Button(text='Generate Password', command=generate_password)
 generate_password_button.grid(column=2, row=3, columnspan=1)
 
 add_button = Button(text='Add', width=43, command=save_password)

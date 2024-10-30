4ae5ef2525d067858da0b8b8216edb5a0688d4b3
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-10-30 23:28:37 +0800

    finished the UI setup

--- Intermediate (Day 15-57)/Intermediate-Day-29/Main-Project/main.py
+++ Intermediate (Day 15-57)/Intermediate-Day-29/Main-Project/main.py
@@ -6,13 +6,39 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Password Manager')
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 # Adding the logo img
 logo_img = (PhotoImage(file='logo.png'))
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 canvas.create_image(100,100, image=logo_img)
-canvas.grid(column=0, row=0)
+canvas.grid(column=1, row=0)
+
+# Adding labels, entries, and buttons
+website_label = Label(text='Website:')
+website_label.grid(column=0, row=1)
+
+website_entry = Entry(width=50)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+email_label = Label(text='Email/Username:')
+email_label.grid(column=0, row=2)
+
+email_entry = Entry(width=50)
+email_entry.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text='Password:')
+password_label.grid(column=0, row=3)
+
+password_entry = Entry(width=32)
+password_entry.grid(column=1, row=3)
+
+generate_password_button = Button(text='Generate Password')
+generate_password_button.grid(column=2, row=3, columnspan=1)
+
+add_button = Button(text='Add', width=43)
+add_button.grid(column=1, row=4, columnspan=2)
+
 
 
 window.mainloop()

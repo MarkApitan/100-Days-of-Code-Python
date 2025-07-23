6fb2710b06a0671f3980bbd7c8996495a5ca600e
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2025-07-23 14:46:50 +0800

    finished step 1 creating the UI

--- Intermediate (Day 15-57)/Intermediate-Day-31/Main-Project/main.py
+++ Intermediate (Day 15-57)/Intermediate-Day-31/Main-Project/main.py
@@ -1,1 +1,30 @@
+from tkinter import *
+# ---------------------------- UI SETUP ------------------------------- #
 BACKGROUND_COLOR = "#B1DDC6"
+window = Tk()
+window.title('Flash Card App')
+window.config(padx=50, pady=50,bg=BACKGROUND_COLOR)
+
+# Images
+right_img = PhotoImage(file=r"c:\Users\User\OneDrive - Polytechnic University of the Philippines\Documents\100-Days-of-Code-Phyton\Intermediate (Day 15-57)\Intermediate-Day-31\Main-Project\images\right.png")
+wrong_img =  PhotoImage(file=r"c:\Users\User\OneDrive - Polytechnic University of the Philippines\Documents\100-Days-of-Code-Phyton\Intermediate (Day 15-57)\Intermediate-Day-31\Main-Project\images\wrong.png")
+
+card_front = PhotoImage(file=r"c:\Users\User\OneDrive - Polytechnic University of the Philippines\Documents\100-Days-of-Code-Phyton\Intermediate (Day 15-57)\Intermediate-Day-31\Main-Project\images\card_front.png")
+card_back =PhotoImage(file=r"c:\Users\User\OneDrive - Polytechnic University of the Philippines\Documents\100-Days-of-Code-Phyton\Intermediate (Day 15-57)\Intermediate-Day-31\Main-Project\images\card_back.png")
+
+# Canvas
+canvas = Canvas(width=800, height=526,bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.create_image(400, 263, image=card_front)
+language_text = canvas.create_text(400, 150, text="French", font=("Arial", 40, "italic"))
+word_text = canvas.create_text(400, 263, text="trouve", font=("Arial", 60, "bold"))
+
+canvas.grid(column=0,row=0,columnspan=2)
+
+# Buttons
+wrong_button = Button(image=wrong_img, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
+wrong_button.grid(column=0, row=1)
+
+right_button = Button(image=right_img, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
+right_button.grid(column=1, row=1)
+
+window.mainloop()
